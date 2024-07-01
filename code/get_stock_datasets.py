54cce7e7a9f2922caf112253839ee3f49b3f9d2a
Liver Szabo <liver.szabo@gmail.com>
2024-07-01 16:23:41 -0400

    June 25, 2024 dataset

--- code/get_stock_datasets.py
+++ code/get_stock_datasets.py
@@ -9,5 +9,5 @@ ticker_symbols = ticker_symbols_string.split(',')
 print(ticker_symbols)
 
 for ticker_symbol in ticker_symbols:
-    data = get_data(ticker_symbol, start_date="06/25/2024", end_date="06/25/2024", index_as_date = True, interval = "1m")
-    data.to_csv('code\\datasets\\output' + ticker_symbol + '.csv')
+    data = get_data(ticker_symbol, start_date="06/25/2024", end_date="06/26/2024", index_as_date = True, interval = "1m")
+    data.to_csv('code\\datasets\\output' + '_06252024_' + ticker_symbol + '.csv')
